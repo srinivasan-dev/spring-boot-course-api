85a93756960b11772a35ec660d047902a9786b50
srinivasan <srini@it-architecturegroup.com>
2020-06-08 01:35:55 +0530

    initial commit - completed course resource with all crud methods

---
+++ src/test/java/com/itag/courseapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itag.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
